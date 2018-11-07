37d2e1ceefe1bc0807bfe3a92872fc237b51608e
Bernardo Peraza <bernardo_peraza@hotmail.com>
2018-11-07 14:59:39 -0600

    Backend - Autenticación con Google Sign-in

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '489047598348-8754f4kuknlpvfeeban0mgmhrjf1acdn.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'rzmhLcezIChG-J8L_srdU3pl';
